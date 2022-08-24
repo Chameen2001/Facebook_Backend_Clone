f7f37a0a86f549139b01d531cde698f16d9c6ebc
Chameen2001 <dilhara1117@gmail.com>
2022-08-25 00:46:19 +0530

    search user by name is implemented

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -51,6 +51,16 @@ router.post("/", async (req, res, next) => {
 
   /****If We use this method(Promising) we do not need to require async and await (Don't forget to remove async in the http method)*/
 
+  // let user = new User({
+  //     firstName: req.body.name.firstName,
+  //     surName: req.body.name.surName,
+  //     mobileNo: req.body.mobileNo,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //     birthDay: req.body.birthDay.date + "-" + req.body.birthDay.month + "-" + req.body.birthDay.year,
+  //     gender: req.body.gender
+  // })
+
   // user.save().then(result=>{
   //     res.send(result);
   // })
@@ -82,6 +92,42 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.get("/", async (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+  try {
+    // res.send(await User.find({},{__v:0})); this is a way to avoiding fetching unwanted data;
+    // res.send(await User.find({},{_id:0})); this is the way to get data without id
+    //res.send(await User.find({},{_id:0,firstName:1}));  if we want only one property, we can use this
+    // res.send(await User.find({firstName:"Chameen"},{})); This is the way to filter
+
+    res.send(await User.find({}, { __v: 0,password:0 }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/search/",async(req,res,next)=>{
+  try {
+
+    // res.send(await User.find({firstName:req.query.username},{}));
+
+    const users = await User.find({},{firstName:1,surName:1,_id:1});
+    const userAr = [];
+    users.forEach((user)=>{
+      console.log(user.firstName + " " + user.surName);
+      if(req.query.firstName === user.firstName || req.query.surName === user.surName){
+        userAr.push({
+          _id:user._id,
+          firstName:user.firstName,
+          surName:user.surName
+        })
+      }
+    });
+
+    res.json(userAr);
+
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
