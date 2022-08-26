0eeee00bba4e7d3ba794c3b4529736ed70b0c327
Chameen2001 <dilhara1117@gmail.com>
2022-08-26 13:02:01 +0530

    main index.js file change to app.js

--- app.js
+++ app.js
@@ -1,14 +1,91 @@
-// const express = require('express');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const userRoute = require('./routes/user.routes');
+const postRoute = require('./routes/post.routes')
+
+// -----------Introducing Content Type to the Express------------------
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+/*****************************************************************/
 
 
 
 
-// const app = express();
 
-// app.use('/user', (req, res) => {
-//   res.send('Hello World!')
-// });
+//------------------Connection to database---------------------------
 
-// // app.listen(port, () => {
-// //   console.log(`Example app listening on port ${port}`)
-// // })
+const dbUrl = 'mongodb+srv://cluster0.189atqv.mongodb.net/';
+const connectionParams = {
+    dbName:'facebook_clone_db',
+    user:'root',
+    pass:'dilhara2001',
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+mongoose.connect(dbUrl,connectionParams)
+.then(()=>{
+  console.log("database connected");
+})
+.catch(()=>{
+  console.log("connecting database is unsuccessful");
+})
+
+/*****************************************************************/
+
+
+
+
+
+
+
+//-----------------Port Listening---------------------------------------
+
+app.listen(PORT,()=>{
+    console.log(`Listening : ${PORT}`);
+});
+
+/**************************************************************** */
+
+
+
+
+
+
+
+
+//--------------Routeing-------------------------------------------------
+
+app.use('/user',userRoute);
+app.use('/post',postRoute);
+
+/**************************************************************** */
+
+
+
+
+
+
+//------------------------------Error Handling------------------------
+
+app.use((req,res,next)=>{
+    const err = new Error('Not Found');
+    err.status=404;
+    next(err);
+})
+
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    res.send({
+        error:{
+            status:err.status || 500,
+            message:err.message
+        }
+    });
+})
+
+/**************************************************************** */
